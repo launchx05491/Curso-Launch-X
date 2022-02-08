7ac9d2534f32f3771c60161b41b2fca571c1c8cc
wazonbhs <adrian_carmona@pime.com.mx>
2022-02-07 23:50:53 -0600

    Initial commit

---
+++ Modulo 1/program.py
@@ -0,0 +1,11 @@
+# program.py
+sum = 1 + 2
+print(sum)
+
+# Impresión en pantalla print('Hola desde la consola')
+print('Hola desde la consola')
+
+# Tu turno, prueba el fragmento de código anterior
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
